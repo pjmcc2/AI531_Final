31406d5da8ce045d783fb82aed0f1f4b14ef41d2
Peter McCabe <pjmcc2@gmail.com>
2024-05-30 17:43:39 -0700

    MAYBE change structure

--- grid/cell.py
+++ grid/cell.py
@@ -1,9 +1,8 @@
-
-class Cell():
-    def __init__(self, left, right, top, bot, val=None,pos=None) -> None:
-        self.left = left
+class Cell:
+    def __init__(self, right, bot, val=None, pos=None) -> None:
+        # self.left = left
         self.right = right
-        self.top = top
+        # self.top = top
         self.bot = bot
         self.val = val
         self.pos = pos
@@ -11,9 +10,13 @@ class Cell():
     def get_row_word(self):
         if self.right:
             prev_letters = self.right.get_row_word()
-        else: 
+        else:
             prev_letters = ""
         return self.val + prev_letters
-    
+
     def get_col_word(self):
-        
+        if self.bot:
+            prev_letters = self.bot.get_row_word()
+        else:
+            prev_letters = ""
+        return self.val + prev_letters
--- grid/grid.py
+++ grid/grid.py
@@ -3,3 +3,30 @@
 # 2. Cells can "see" their entire column
 # 3. Cells use their knowledge to check against the dictionary
 # 4. Cells should be easily accessible (indexed?)
+
+from cell import Cell
+
+class Grid():
+    def __init__(self,size) -> None:
+        self.size = size
+        self.root = Cell(None,None,pos=0)
+        
+        def gen_row(self,curr):
+            for i in range(1,self.size):
+                if curr.right is None:
+                    temp = Cell(None,None,pos=curr.pos + 1)
+                    curr.right = temp
+                    curr = temp
+
+        def gen_col(self):
+            for i in range(1,self.size):
+                if curr.bot is None:
+                    temp = Cell(None,None,pos=curr.pos + self.size)
+                    curr.bot = temp
+                    curr = temp
+        
+
+
+    def set_word(self,word) -> None:
+
+        
